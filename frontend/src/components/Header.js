1eceacf8d2969b2b6f19d29aca603d5755d4d8cb
alejandroToledo <alejandrotol.her@gmail.com>
2020-09-13 15:52:38 -0300

    cambios en los estilos

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,12 +4,23 @@ import "../styles/styles.css"
 const Header = () => {
     return (
         <header>
-            <NavLink to="/"> <h2>ESCAPE</h2></NavLink>
-            <nav>
+            <NavLink to="/"> <h2>SCAPE</h2></NavLink>
+            <nav className="d-flex">
                 <NavLink to="/news">News</NavLink>
                 <NavLink to="/games">Games</NavLink>
-                <NavLink to="/signup">Sign Up</NavLink>
-                <NavLink to="/login">Log in</NavLink>
+
+                <div class="dropdown show " >
+                    <a class="btn btn-secondary dropdown-toggle-left " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        pic</a>
+
+                    <div class="dropdown-menu " aria-labelledby="dropdownMenuLink">
+                        <div class=" d-flex flex-column">
+                            <NavLink to="/signup">Sign Up</NavLink>
+                            <NavLink to="/login">Log in</NavLink>
+
+                        </div>
+                    </div>
+                </div>
             </nav>
         </header>
     )
